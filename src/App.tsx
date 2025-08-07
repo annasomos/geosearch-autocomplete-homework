c0f5f17b6596aa30e3f2af7152e8a1027faa70a4
annasomos <annasomoss@gmail.com>
2025-08-07 23:18:24 +0200

    feat: add logic to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "@mantine/core/styles.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Card, Group, MantineProvider, Text, Image } from "@mantine/core";
+import GeoSearchAutoComplete from "./components/GeoSearchAutoComplete.tsx";
+import { useGeoSearchQuery } from "./hooks/useGeoSearchQuery.tsx";
+import { useRef, useState } from "react";
+
+export default function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [debouncedSearch, setDebouncedSearch] = useState("");
+  const timerRef = useRef<number>(0);
+
+  const { data, isLoading, error } = useGeoSearchQuery({
+    searchTerm: debouncedSearch,
+  });
+
+  function createAutoCompleteList() {
+    if (!data) return [];
+    const filteredData = data.filter((loc) => loc.population >= 500000);
+    const options = filteredData.map((loc) => {
+      const label = `${loc.name} ${loc.admin1 ? ", " + loc.admin1 : ""}${
+        loc.country ? ", " + loc.country : ""
+      }`;
+      const uniqueKey = loc.id.toString();
+
+      return {
+        value: uniqueKey,
+        label,
+      };
+    });
+
+    return options;
+  }
+  function handleChange(value: string) {
+    setInputValue(value);
+    clearTimeout(timerRef.current);
+    timerRef.current = setTimeout(() => {
+      clearTimeout(timerRef.current);
+      setDebouncedSearch(value);
+    }, 500);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <MantineProvider>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <Card
+          shadow="sm"
+          padding="lg"
+          radius="md"
+          withBorder
+          style={{ maxWidth: 600, width: "100%", height: 325 }}
+        >
+          <Card.Section>
+            <Image
+              src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
+              height={160}
+            />
+          </Card.Section>{" "}
+          <Group justify="space-between" mt="md" mb="xs">
+            <Text fw={500}>GeoLocation Autocomplete Component</Text>
+          </Group>
+          <GeoSearchAutoComplete
+            value={inputValue}
+            data={createAutoCompleteList()}
+            error={error}
+            isLoading={isLoading}
+            onChange={handleChange}
+          />
+        </Card>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </MantineProvider>
+  );
 }
-
-export default App
